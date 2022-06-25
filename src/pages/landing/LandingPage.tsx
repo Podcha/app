cac70332473d000fb7903a506ecea00225af1fb1
keesvanbochove <keesvanbochove@gmail.com>
2022-06-26 01:13:36 +0200

    Adding landing page content

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+
 export function LandingPage() {
-  return <div>LandingPage</div>;
+  return (<div>
+    <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w">
+      <h1 className="text-5xl font-bold">Your favourite podcasts.</h1>
+      <h1 className="text-5xl font-bold text-sky-500">On chain.</h1>
+      <p className="py-6">Decentralized platform where you can find, collect and listen to the world’s best podcasts.</p>
+      <Link to="/podcasts"><button className="btn btn-primary">Get Started</button></Link>
+    </div>
+  </div>
+</div>
+  </div>);
 }
