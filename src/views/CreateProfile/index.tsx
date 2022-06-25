2b6456a4fca8d962d482298bf7d099ced913a35f
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-06-25 12:08:32 -0400

    Types added

--- src/views/CreateProfile/index.tsx
+++ src/views/CreateProfile/index.tsx
@@ -7,11 +7,19 @@ import {
   LensHubProxyAddress,
   freeCollectModuleAddress,
 } from "../../consts";
-import mockProfileABI from "../../abi/mockProfileABI.json";
-import LensHubABI from "../../abi/LensHubABI.json";
+import {
+  LensHub__factory,
+  LensHub,
+  MockProfileCreationProxy__factory,
+  MockProfileCreationProxy,
+} from "../../contracts/Lens";
 
 const CreateProfile = () => {
   const [user, setUser] = useState(null);
+  const [userHandle, setUserHandle] = useState<string>("");
+  const [lensHubContract, setLensHubContract] = useState<LensHub>();
+  const [lensMockProfileContract, setLensMockProfileContract] =
+    useState<MockProfileCreationProxy>();
   const [profiles, setProfiles] = useState<any[]>([]);
 
   const connect = async () => {
@@ -23,19 +31,9 @@ const CreateProfile = () => {
   };
 
   const profileCreate = async () => {
-    /* @ts-ignore */
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-
-    const contract = new ethers.Contract(
-      mockProfileAddress,
-      mockProfileABI,
-      signer
-    );
-
     const inputStruct = {
       to: user,
-      handle: "handleName",
+      handle: userHandle,
       imageURI:
         "https://ipfs.io/ipfs/QmY9dUwYu67puaWBMxRKW98LPbXCznPwHUbhX5NeWnCJbX",
       followModule: ethers.constants.AddressZero,
@@ -45,83 +43,74 @@ const CreateProfile = () => {
     };
 
     try {
-      const tx = await contract.proxyCreateProfile(inputStruct);
+      /* @ts-ignore */
+      const tx = await lensMockProfileContract.proxyCreateProfile(inputStruct);
       await tx.wait();
     } catch (error) {
       console.error({ error });
     }
   };
 
-  const setDefaultProfile = async () => {
-    /* @ts-ignore */
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
+  // const setDefaultProfile = async () => {
+  //   try {
+  //     const tx = await lensHubContract.setDefaultProfile(
+  //       ethers.BigNumber.from("lens-profile-id")
+  //     );
+  //     await tx.wait();
+  //   } catch (error) {
+  //     console.error({ error });
+  //   }
+  // };
 
-    const contract = new ethers.Contract(
-      LensHubProxyAddress,
-      LensHubABI,
-      signer
-    );
+  // const post = async () => {
+  //   const postStruct = {
+  //     profileId: ethers.BigNumber.from("lens-profile-id"),
+  //     contentURI:
+  //       "https://ipfs.io/ipfs/Qmby8QocUU2sPZL46rZeMctAuF5nrCc7eR1PPkooCztWPz",
+  //     collectModule: freeCollectModuleAddress,
+  //     collectModuleInitData: ethers.utils.defaultAbiCoder.encode(
+  //       ["bool"],
+  //       [true]
+  //     ),
+  //     referenceModule: ethers.constants.AddressZero,
+  //     referenceModuleInitData: [],
+  //   };
 
-    try {
-      const tx = await contract.setDefaultProfile(
-        ethers.BigNumber.from("lens-profile-id")
-      );
-      await tx.wait();
-    } catch (error) {
-      console.error({ error });
-    }
-  };
-
-  const post = async () => {
-    /* @ts-ignore */
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-
-    const contract = new ethers.Contract(
-      LensHubProxyAddress,
-      LensHubABI,
-      signer
-    );
-
-    const postStruct = {
-      profileId: ethers.BigNumber.from("lens-profile-id"),
-      contentURI:
-        "https://ipfs.io/ipfs/Qmby8QocUU2sPZL46rZeMctAuF5nrCc7eR1PPkooCztWPz",
-      collectModule: freeCollectModuleAddress,
-      collectModuleInitData: ethers.utils.defaultAbiCoder.encode(
-        ["bool"],
-        [true]
-      ),
-      referenceModule: ethers.constants.AddressZero,
-      referenceModuleInitData: [],
-    };
-
-    try {
-      const tx = await contract.post(postStruct);
-      await tx.wait();
-    } catch (error) {
-      console.error({ error });
-    }
-  };
-
-  const fetchProfile = async () => {
-    try {
-      const response = await client
-        .query(getProfiles, { address: user })
-        .toPromise();
-      console.log(response);
-
-      setProfiles([...(response.data.profiles?.items || null)]);
-    } catch (error) {
-      console.error({ error });
-    }
-  };
+  //   try {
+  //     const tx = await lensHubContract.post(postStruct);
+  //     await tx.wait();
+  //   } catch (error) {
+  //     console.error({ error });
+  //   }
+  // };
 
   useEffect(() => {
     if (!user) return;
 
-    fetchProfile();
+    const fetchProfilesAndSetContracts = async () => {
+      try {
+        const response = await client
+          .query(getProfiles, { address: user })
+          .toPromise();
+        console.log(response);
+
+        /* @ts-ignore */
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        setProfiles([...(response.data.profiles?.items || null)]);
+        setLensHubContract(
+          LensHub__factory.connect(LensHubProxyAddress, signer)
+        );
+        setLensMockProfileContract(
+          MockProfileCreationProxy__factory.connect(mockProfileAddress, signer)
+        );
+      } catch (error) {
+        console.error({ error });
+      }
+    };
+
+    fetchProfilesAndSetContracts();
   }, [user]);
 
   if (!user) {
@@ -131,18 +120,24 @@ const CreateProfile = () => {
   return (
     <div>
       {user}
-      {!profiles ? (
+      <div>
+        <input
+          type="text"
+          onChange={(event) => setUserHandle(event.target.value)}
+          value={userHandle}
+        />
         <button onClick={profileCreate}>Create</button>
-      ) : (
-        <div>
-          List of your profiles:
-          {profiles.map((profile) => (
-            <p>{profile.handle}</p>
-          ))}
-        </div>
-      )}
+        {profiles && (
+          <div>
+            List of your profiles:
+            {profiles.map((profile) => (
+              <p key={profile.handle}>{profile.handle}</p>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
-};
+};;;;;
 
 export default CreateProfile;
