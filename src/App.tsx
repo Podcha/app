97351e3a8cbe63f70de66d551a05e5e1bf5868c4
keesvanbochove <keesvanbochove@gmail.com>
2022-06-25 13:58:20 +0200

    Adding DaisyUI and some initial app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import './app.css'
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+  return (
+
+    <div className="navbar flex-5 bg-base-100 rounded-box">
+
+      <a className="btn btn-ghost normal-case text-xl">Podcha</a>
+
+    
+
+      <ul className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-152">
+        <li><a>Podcasts</a></li>
+        <li><a>Episodes</a></li>
+        <li><a>My show</a></li>
+
+
+      </ul>
+
+      <div className="dropdown dropdown-end">
+      
+      
+
+      <button className="btn justify-right"><label className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="https://api.lorem.space/image/face?hash=33791" />
+        </div>
+      </label>@lensrocks</button>
+
+      </div>
+ 
+    </div>
+
+    );
 }
 
 export default App;
