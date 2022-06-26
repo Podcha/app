08d3345d57d763094a9ae1066683440e5cd8c5d8
keesvanbochove <keesvanbochove@gmail.com>
2022-06-26 06:57:38 +0200

    Extend landing page

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import Player from "lottie-react";
+import podcastgirl from './51643-podcast-girl.json';
+import datacenter from './67171-data-center.json';
 
 export function LandingPage() {
   return (
@@ -41,14 +44,49 @@ export function LandingPage() {
 
       </div>
 
-      <div className="min-h-screen bg-base-400">
+      <div className="bg-base-200 min-h-screen flex flex-col place-content-between">
 
-<img src="podcast-girl.gif"></img>
-      <div className="grid grid-cols-1 max-w place-items-center text-center space-y-8">
-          <h1 className="max-w-md justify-center text-3xl font-bold">Create your own <span className="text-sky-500">monetization</span> strategy.</h1>
-          <h1 className="max-w-md py-6">As a creator, you can configure your own monetization strategy: free or paid subscription on a podcast level, and even micropayments on an episode level. Experiment with free and premium content, engage your audience and create your own content market!</h1>
+        <div className="flex flex-row align-start">
+
+          <Player
+            animationData={podcastgirl}
+            style={{ height: '300px', width: '300px' }}
+          >
+          </Player>
+
+          <div className="flex flex-col max-w place-items-center text-center space-y-8">
+            <h1 className="max-w-md justify-center text-3xl font-bold">Create your own <span className="text-sky-500">monetization</span> strategy.</h1>
+            <h1 className="max-w-md py-6">As a creator, you can configure your own monetization strategy: free or paid subscription on a podcast level, and even micropayments on an episode level. Experiment with free and premium content, engage your audience and create your own content market!</h1>
+          </div>
+
+
+
+        </div>
+        <div className="flex flex-row-reverse">
+
+          <div className="flex flex-col max-w place-items-center text-center space-y-8">
+            <h1 className="max-w-md justify-center text-3xl font-bold">Be in <span className="text-sky-500">control</span> of your data.</h1>
+            <h1 className="max-w-md py-6">The social graph is on the blockchain, and your audio or video is stored and hosted in a peer to peer platform.</h1>
+          </div>
+
+          <Player
+            animationData={datacenter}
+            style={{ height: '300px', width: '300px' }}
+          >
+          </Player>
+
+        </div>
+      </div>
+
+      <div className="min-h-screen bg-base-400 grid grid-col p-20">
+        <div className="flex flex-col">
+          <p className="max-w-md place-self-center text-center text-3xl font-bold">Built with</p>
+          <img src="Sponsors.png"></img>
         </div>
 
+          <p className="max-w-md align-bottom place-self-center text-center text-3xl font-bold">Let’s take back control over podcasts together!</p>
+
+       
       </div>
 
     </div>
